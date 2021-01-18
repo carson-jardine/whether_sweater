d6c2f30d1900924ae505b11b97bb8d60f2be9c75
Carson Jardine <carsonjardine@gmail.com>
2021-01-18 11:51:45 -0700

    Add open weather and unsplash to filter sensitive data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,4 +78,6 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data('<MAPQUEST_KEY>') { ENV['MAPQUEST_API_KEY'] }
+  config.filter_sensitive_data('<OPEN_WEATHER_KEY>') { ENV['OPEN_WEATHER_API_KEY'] }
+  config.filter_sensitive_data('<UNSPLASH_KEY>') { ENV['UNSPLASH_API_KEY'] }
 end
