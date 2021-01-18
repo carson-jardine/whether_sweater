2cb4af17d4b2cfb0a0c06324180767c6e495f3e8
Carson Jardine <carsonjardine@gmail.com>
2021-01-18 10:21:14 -0700

    Add vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ require 'simplecov'
 
 SimpleCov.start
 
-
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -77,4 +76,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<MAPQUEST_KEY>') { ENV['MAPQUEST_API_KEY'] }
 end
