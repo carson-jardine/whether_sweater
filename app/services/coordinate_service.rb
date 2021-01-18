8dd62a21e8518b22ba15141f8e469c112f08bb0e
Carson Jardine <carsonjardine@gmail.com>
2021-01-18 10:25:00 -0700

    Test and code for coordinate service

---
+++ app/services/coordinate_service.rb
@@ -0,0 +1,22 @@
+class CoordinateService
+  def self.get_coordinates(location)
+    response = conn.get('address') do |req|
+      req.params['location'] = location
+      req.params['maxResults'] = 1
+    end
+    data = parse_data(response)
+    data[:results][0][:locations][0][:latLng]
+  end
+
+  def self.parse_data(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new('http://www.mapquestapi.com/geocoding/v1') do |conn|
+      conn.params['key'] = ENV['MAPQUEST_API_KEY']
+    end
+  end
+end
