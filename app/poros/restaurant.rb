b02ac67352b2eb39c001cc519eeee925be08840a
Carson Jardine <carsonjardine@gmail.com>
2021-01-19 11:29:44 -0700

    Update restaurant poro

---
+++ app/poros/restaurant.rb
@@ -0,0 +1,19 @@
+class Restaurant
+  attr_reader :id,
+              :restaurant_details
+
+  def initialize(restaurant_data)
+    @id = nil
+    @restaurant_details = format_restaurant_return(restaurant_data)
+  end
+
+  def format_restaurant_return(restaurant_data)
+    data = restaurant_data[:businesses][0]
+    return {
+     restaurant: {
+        name: data[:name],
+        address: data[:location][:display_address].join(', ')
+      }
+    }
+  end
+end
