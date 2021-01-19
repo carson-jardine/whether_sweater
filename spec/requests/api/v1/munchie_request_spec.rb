fba932c9e20c7d2791cfd9ebee9bce561bd7a942
Carson Jardine <carsonjardine@gmail.com>
2021-01-19 09:27:37 -0700

    Add further testing for munchie request

--- spec/requests/api/v1/munchie_request_spec.rb
+++ spec/requests/api/v1/munchie_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Munchie Request' do
   describe 'When I input valid information' do
-    it 'retrieves food and forecast info for a destination city' do
+    it 'retrieves food and forecast info for a destination city', :vcr do
       start_city = 'denver,co'
       end_city = 'pueblo,co'
       food = 'chinese'
@@ -11,6 +11,35 @@ RSpec.describe 'Munchie Request' do
       expect(response).to be_successful
       expect(response.status).to eq(200)
       munchie_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(munchie_data).to have_key(:id)
+      expect(munchie_data[:id]).to eq(nil)
+
+      expect(munchie_data).to have_key(:type)
+      expect(munchie_data[:type]).to eq('munchie')
+
+      expect(munchie_data).to have_key(:attributes)
+      expect(munchie_data[:attributes]).to be_a(Hash)
+
+      expect(munchie_data[:attributes]).to have_key(:destination_city)
+      expect(munchie_data[:attributes][:destination_city]).to eq('Pueblo, CO')
+
+      expect(munchie_data[:attributes]).to have_key(:travel_time)
+      expect(munchie_data[:attributes][:travel_time]).to be_a(String)
+
+      expect(munchie_data[:attributes]).to have_key(:forecast)
+      expect(munchie_data[:attributes][:forecast]).to be_a(Hash)
+      expect(munchie_data[:attributes][:forecast]).to have_key(:summary)
+      expect(munchie_data[:attributes][:forecast][:summary]).to be_a(String)
+      expect(munchie_data[:attributes][:forecast]).to have_key(:temperature)
+      expect(munchie_data[:attributes][:forecast][:temperature]).to be_a(String)
+
+      expect(munchie_data[:attributes]).to have_key(:restaurant)
+      expect(munchie_data[:attributes][:restaurant]).to be_a(Hash)
+      expect(munchie_data[:attributes][:restaurant]).to have_key(:name)
+      expect(munchie_data[:attributes][:restaurant][:name]).to be_a(String)
+      expect(munchie_data[:attributes][:restaurant]).to have_key(:address)
+      expect(munchie_data[:attributes][:restaurant][:address]).to be_a(String)
     end
   end
 end
