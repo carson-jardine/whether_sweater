f64376d6c76b932ae1e0041ce8346f5ee0aeebe0
Carson Jardine <carsonjardine@gmail.com>
2021-01-20 01:40:55 -0700

    Pass all road trip tests 100% coverage

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::RoadTripController < ApplicationController
     road_trip_data = JSON.parse(request.body.string, symbolize_names: true)
 
     user = User.find_by(api_key: road_trip_data[:api_key])
-    trip_params = { origin: road_trip_data[:origin], destination: road_trip_data[:destination] }
-    road_trip = RoadTripFacade.create_road_trip(trip_params)
     if user
+      trip_params = { origin: road_trip_data[:origin], destination: road_trip_data[:destination] }
+      road_trip = RoadTripFacade.create_road_trip(trip_params)
       render json: RoadTripSerializer.new(road_trip)
     end
   end
--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -6,25 +6,29 @@ class RoadTripFacade
   end
 
   def self.weather_at_eta(trip_data)
-    require "pry"; binding.pry
-    
     location = trip_data[:destination]
     forecast = ForecastFacade.get_forecast(location)
     hour_of_arrival = find_arrival_time(trip_data)
-    forecast_at_arrival = forecast.hourly_weather.find do |hourly|
-      hourly[:time] == hour_of_arrival
+    if hour_of_arrival != 'Impossible Route'
+      forecast_at_arrival = forecast.hourly_weather.find do |hourly|
+        hourly[:time] == hour_of_arrival
+      end
+      {
+        conditions: forecast_at_arrival[:conditions],
+        temperature: forecast_at_arrival[:temperature]
+      }
     end
-    {
-      conditions: forecast.hourly_weather[:conditions],
-      temperature: forecast.hourly_weather[:temperature]
-    }
   end
 
   def self.find_arrival_time(trip_data)
     current_time = Time.now.to_i
     travel_time = travel_time(trip_data)
-    arrival = Time.strptime((current_time + travel_time[:real_time]).to_s, '%s').beginning_of_hour
-    arrival.strftime('%T')
+    if travel_time[:real_time].nil? || travel_time[:real_time] >= 10000000
+      return 'Impossible Route'
+    else
+      arrival = Time.strptime((current_time + travel_time[:real_time]).to_s, '%s').beginning_of_hour
+      arrival.strftime('%T')
+    end
   end
 
   def self.travel_time(trip_data)
