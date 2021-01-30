a80bdce07ccb6e08c49346470f8c88fa7acf8fc9
Carson Jardine <carsonjardine@gmail.com>
2021-01-30 13:52:15 -0700

    Test for travis

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     road_trip_data = JSON.parse(request.body.string, symbolize_names: true)
 
-    user = User.find_by(api_key: road_trip_data[:api_key])
+    # user = User.find_by(api_key: road_trip_data[:api_key])
     if user
       trip_params = { origin: road_trip_data[:origin], destination: road_trip_data[:destination] }
       road_trip = RoadTripFacade.create_road_trip(trip_params)
